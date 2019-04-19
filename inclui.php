17faea93feba6eabe9a29a5753299f8bd2adf696
LeoAmendola <49735722+LeoAmendola@users.noreply.github.com>
2019-04-19 11:29:39 -0300

    tela de inclusão funcionando

---
+++ inclui.php
@@ -0,0 +1,22 @@
+    
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset=utf8>
+    <title>Inclusão de novos usuários</title>
+</head>
+<body>
+<form action=salvar.php>
+<p>Nome</p>
+<input type=text name=nome1>
+<p>Data_Nascimento</p>
+<input type=date name=datanasc1>
+<p>Salário</p>
+<input type=number name=salario1>
+<br><br>
+<input type=submit value=Salvar>
+</form> 
+<br><br>
+<a href=index.php>Voltar</a>   
+</body>
+</html>
